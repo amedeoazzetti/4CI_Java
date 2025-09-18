cf28fe6af7d82aab3f462e4fafc4c18f0e0a52d7
Amedeo Azzetti <amedeo.azzetti@marconirovereto.it>
2025-09-18 11:22:12 +0200

    aggiunto costruttore di Giocatore

--- GDR/src/Giocatore.java
+++ GDR/src/Giocatore.java
@@ -1,9 +1,26 @@
+
+import java.util.ArrayList;
+
 public abstract class Giocatore{
 
     protected String name;
-    protected String hp;
+    protected int hp;
     protected final int HP_MAX, MANA_MAX, PESO_MAX;
     protected int mana;
     protected int peso;
+    protected Razza razza;
+    protected ArrayList<Equip> inventario;
+
+    public Giocatore(int hp, int mana, String name, int peso, Razza razza) {
+        this.hp = this.HP_MAX = hp;
+        this.mana = this.MANA_MAX = mana;
+        this.name = name;
+        this.peso = this.PESO_MAX = peso;
+        this.razza = razza;
+
+        this.inventario = new ArrayList<>();
+    }
+
+
 
 }
--- GDR/src/TipoEquip.java
+++ GDR/src/TipoEquip.java
@@ -1,3 +1,3 @@
-public class TipoEquip {
-    
+public enum TipoEquip {
+    Arma, Armatura, Pozione, Misc
 }
