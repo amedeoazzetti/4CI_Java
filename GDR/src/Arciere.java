94c427c3a070c6872c906c4cd72c8a12a9daf346
Amedeo Azzetti <amedeo.azzetti@marconirovereto.it>
2025-09-25 10:43:13 +0200

    aggiunto il metodo attacca all'Arciere

--- GDR/src/Arciere.java
+++ GDR/src/Arciere.java
@@ -5,5 +5,34 @@ public class Arciere extends Giocatore {
         super(hp, mana, nome, peso, razza);
     }
 
-    
+    @Override
+    public int
+            attacca(Giocatore target, int danno) {
+        // verifico se il target ha armatura
+        int armatura = 0;
+
+        for (Equip equip : target.getInventario()) {
+            if (equip.getTipo() == TipoEquip.Armatura) {
+                armatura++;
+            }
+            target.setHp(target.getHp() - danno / (armatura + 1));
+        }
+
+      
+
+        int multi = 1;
+        if (target instanceof Guerriero || target instanceof Mago) {
+            multi = 2;
+        }
+
+        // modifico i puntivita del target
+        int dannoFinale = multi * danno / (armatura + 1);
+        target.setHp(target.getHp() - dannoFinale);
+
+
+        // se ho sconfitto qualcuno, ricarico
+        
+        return dannoFinale;
+
+ }
 }
