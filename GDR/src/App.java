2b3addea3275eb31fda76bf230f681cdff497b15
Amedeo Azzetti <amedeo.azzetti@marconirovereto.it>
2025-09-25 11:26:02 +0200

    aggiunto nuovo giocatore Sauron e medico Rada; implementate interazioni di attacco e rianimazione

--- GDR/src/App.java
+++ GDR/src/App.java
@@ -1,12 +1,13 @@
-
 public class App {
-
     public static void main(String[] args) throws Exception {
+        
         Squadra team = new Squadra();
 
-        Giocatore aragorn = new Guerriero("Aragor", 100, 0, 80, Razza.Umano);
-        Giocatore gandalf = new Mago("Ganalf", 70, 100, 20, Razza.Ent);
+        Giocatore aragorn = new Guerriero("Aragorn", 100, 0, 80, Razza.Umano);
+        Giocatore gandalf = new Mago("Gandalf", 70, 100, 20, Razza.Ent);
+        Giocatore sauron = new Mago("Sauron", 70, 1000, 200, Razza.Orco);
         Giocatore legolas = new Arciere("Legolas", 90, 0, 50, Razza.Elfo);
+        Giocatore rada = new Medico("Rada", 70, 150, 30, Razza.Umano);
 
         team.aggiungiGiocatore(aragorn);
         team.aggiungiGiocatore(gandalf);
@@ -17,17 +18,34 @@ public class App {
         System.out.println("Battaglia");
         Equip spada = new Equip("Anduril", 20, TipoEquip.ArmaMelee);
         aragorn.aggiungiEquip(spada);
-        System.out.println("Peso inventario: " + aragorn.getPeso());
+        System.out.println("Peso inventario: " + aragorn.getPeso()); 
 
+        // TODO
         Equip elmo = new Equip("Elmo", 5, TipoEquip.Armatura);
-
         Equip armor = new Equip("Armatura", 30, TipoEquip.Armatura);
+        legolas.aggiungiEquip(armor);
+        legolas.aggiungiEquip(elmo);
 
-        aragorn.aggiungiEquip(elmo);
-        gandalf.aggiungiEquip(armor);
-
-        gandalf.attacca(aragorn, 30);
+        int danno = aragorn.attacca(legolas, 90);
+        System.out.printf("Inflitti %d danni\n", danno);
         team.salutaTutti();
-        
+
+        int danno2 = gandalf.attacca(aragorn, 30);
+        System.out.printf("Inflitti %d danni\n", danno2);
+        int danno3 = gandalf.attacca(sauron, 50);
+        System.out.printf("Inflitti %d danni\n", danno3);
+        team.salutaTutti();
+
+            // rianima (downcasting)
+
+            Medico rad = ((Medico)rada);
+            rad.rianima(legolas);
+
+            // cura
+
+
+            System.out.println(legolas.getHp());
+            rada.attacca(legolas, 50);
+            System.out.println(legolas.getHp());
     }
 }
