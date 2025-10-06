ccb14848cfc2a3dc5361e00a1577e5f6b63e9f9e
Amedeo Azzetti <amedeo.azzetti@marconirovereto.it>
2025-10-06 10:49:41 +0200

    Progetto finito

--- Rogue/src/App.java
+++ Rogue/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        /*Room r1;
+        //Room r1 = new Room(null);
+        Room r2 = new BossRoom(null);
+        Room r3 = new ChallengeRoom(null);
+        BossRoom r4 = new SuperBossRoom(null);
+        Object r5 = new BossRoom(null);
+        //BossRoom r6 = new ChallengeRoom(passive);
+
+        r2.indentify();
+        r4.indentify();
+        ((Room)r5).indentify();
+
+     */
+        
+
     }
 }
